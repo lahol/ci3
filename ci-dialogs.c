6efaa6c5f30686e22af9fbf869342d8051239e36
Holger Langenau <holger.langenau@s2004.tu-chemnitz.de>
2013-09-08 21:46:22 +0200

    Use GtkTable instead of GtkGrid for Gtk < 3.0.

--- ci-dialogs.c
+++ ci-dialogs.c
@@ -14,7 +14,6 @@ gboolean ci_dialogs_add_caller(CICallerInfo *caller)
             NULL);
 
     /* grid */
-    GtkWidget *grid = gtk_grid_new();
     GtkWidget *label, *content_area;
     GtkWidget *entries[2];
     GtkEntryBuffer *buffers[2];
@@ -26,6 +25,8 @@ gboolean ci_dialogs_add_caller(CICallerInfo *caller)
     entries[1] = gtk_entry_new_with_buffer(buffers[1]);
 
     /* padding, align */
+#if GTK_CHECK_VERSION(3, 0, 0)
+    GtkWidget *grid = gtk_grid_new();
     label = gtk_label_new("Number:");
     gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), entries[0], 1, 0, 1, 1);
@@ -33,6 +34,16 @@ gboolean ci_dialogs_add_caller(CICallerInfo *caller)
     label = gtk_label_new("Name:");
     gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), entries[1], 1, 1, 1, 1);
+#else
+    GtkWidget *grid = gtk_table_new(2, 2, FALSE);
+    label = gtk_label_new("Number:");
+    gtk_table_attach_defaults(GTK_TABLE(grid), label, 0, 1, 0, 1);
+    gtk_table_attach_defaults(GTK_TABLE(grid), entries[0], 1, 2, 0, 1);
+
+    label = gtk_label_new("Name:");
+    gtk_table_attach_defaults(GTK_TABLE(grid), label, 0, 1, 1, 2);
+    gtk_table_attach_defaults(GTK_TABLE(grid), entries[1], 1, 2, 1, 2);
+#endif
 
     gtk_widget_show_all(grid);
 
