1782ea5a8dba71663c998b18a844b9f75babc47e
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2013-09-12 09:26:08 +0200

    More structure for config.

--- ci-config.c
+++ ci-config.c
@@ -8,15 +8,18 @@
 #include "gtk2-compat.h"
 
 struct CIConfig {
-    gchar *host;
-    guint port;
-    GdkRGBA background;
-    gint win_x;
-    gint win_y;
-    gint win_w;
-    gint win_h;
-    gint retry_interval;
-    gint user;
+    gchar *general_output;
+
+    gchar *client_host;
+    guint client_port;
+    gint client_retry_interval;
+    gint client_user;
+
+    gint window_x;
+    gint window_y;
+    gint window_width;
+    gint window_height;
+    GdkRGBA window_background;
 } ci_config;
 
 gchar *ci_color_to_string(GdkRGBA *color)
@@ -48,19 +51,24 @@ gchar *ci_config_get_config_file(void)
 
 void ci_config_set_defaults(void)
 {
-    g_free(ci_config.host);
-    ci_config.host = g_strdup("localhost");
-    ci_config.port = 63690;
-    ci_config.win_x = 0;
-    ci_config.win_y = 0;
-    ci_config.win_w = 200;
-    ci_config.win_h = 100;
-    ci_config.retry_interval = 10;
-    ci_config.user = 0;
-    ci_string_to_color(&ci_config.background, "#ffffff");
+    g_free(ci_config.general_output);
+    ci_config.general_output = g_strdup("default");
+
+    g_free(ci_config.client_host);
+    ci_config.client_host = g_strdup("localhost");
+    ci_config.client_port = 63690;
+    ci_config.client_retry_interval = 10;
+    ci_config.client_user = 0;
+
+    ci_config.window_x = 10;
+    ci_config.window_y = 60;
+    ci_config.window_width = 200;
+    ci_config.window_height = 100;
+    ci_string_to_color(&ci_config.window_background, "#ffffff");
 }
 
 gboolean ci_config_load_root(JsonNode *node);
+gboolean ci_config_load_general(JsonNode *node);
 gboolean ci_config_load_client(JsonNode *node);
 gboolean ci_config_load_window(JsonNode *node);
 gboolean ci_config_load_element_array(JsonNode *node);
@@ -70,6 +78,7 @@ gboolean ci_config_load_list_column_array(JsonNode *node);
 gboolean ci_config_load_list_column(JsonNode *node);
 
 JsonNode *ci_config_save_root(void);
+void ci_config_save_general(JsonBuilder *builder);
 void ci_config_save_client(JsonBuilder *builder);
 void ci_config_save_window(JsonBuilder *builder);
 void ci_config_save_element_array(JsonBuilder *builder);
@@ -134,32 +143,50 @@ gboolean ci_config_save(void)
 
 gboolean ci_config_get(const gchar *key, gpointer value)
 {
-    if (g_strcmp0(key, "client:host") == 0) {
-        *((gchar**)value) = g_strdup(ci_config.host);
+    if (g_str_has_prefix(key, "general:")) {
+        if (g_strcmp0(&key[8], "output") == 0) {
+            if (ci_config.general_output != NULL)
+                *((gchar **)value) = g_strdup(ci_config.general_output);
+            else
+                *((gchar **)value) = g_strdup("default");
+        }
+        else
+            return FALSE;
     }
-    else if (g_strcmp0(key, "client:port") == 0) {
-        *((guint*)value) = ci_config.port;
+    else if (g_str_has_prefix(key, "client:")) {
+        if (g_strcmp0(&key[7], "host") == 0) {
+            *((gchar**)value) = g_strdup(ci_config.client_host);
+        }
+        else if (g_strcmp0(&key[7], "port") == 0) {
+            *((guint*)value) = ci_config.client_port;
+        }
+        else if (g_strcmp0(&key[7], "retry-interval") == 0) {
+            *((gint*)value) = ci_config.client_retry_interval;
+        }
+        else if (g_strcmp0(&key[7], "user") == 0) {
+            *((gint*)value) = ci_config.client_user;
+        }
+        else
+            return FALSE;
     }
-    else if (g_strcmp0(key, "window:background") == 0) {
-        memcpy(value, &ci_config.background, sizeof(GdkRGBA));
-    }
-    else if (g_strcmp0(key, "window:x") == 0) {
-        *((gint*)value) = ci_config.win_x;
-    }
-    else if (g_strcmp0(key, "window:y") == 0) {
-        *((gint*)value) = ci_config.win_y;
-    }
-    else if (g_strcmp0(key, "window:width") == 0) {
-        *((gint*)value) = ci_config.win_w;
-    }
-    else if (g_strcmp0(key, "window:height") == 0) {
-        *((gint*)value) = ci_config.win_h;
-    }
-    else if (g_strcmp0(key, "client:retry-interval") == 0) {
-        *((gint*)value) = ci_config.retry_interval;
-    }
-    else if (g_strcmp0(key, "client:user") == 0) {
-        *((gint*)value) = ci_config.user;
+    else if (g_str_has_prefix(key, "window:")) {
+        if (g_strcmp0(&key[7], "background") == 0) {
+            memcpy(value, &ci_config.window_background, sizeof(GdkRGBA));
+        }
+        else if (g_strcmp0(&key[7], "x") == 0) {
+            *((gint*)value) = ci_config.window_x;
+        }
+        else if (g_strcmp0(&key[7], "y") == 0) {
+            *((gint*)value) = ci_config.window_y;
+        }
+        else if (g_strcmp0(&key[7], "width") == 0) {
+            *((gint*)value) = ci_config.window_width;
+        }
+        else if (g_strcmp0(&key[7], "height") == 0) {
+            *((gint*)value) = ci_config.window_height;
+        }
+        else
+            return FALSE;
     }
     else if (g_strcmp0(key, "config-file") == 0) {
         *((gchar **)value) = ci_config_get_config_file();
@@ -167,42 +194,63 @@ gboolean ci_config_get(const gchar *key, gpointer value)
     else {
         return FALSE;
     }
+
     return TRUE;
 }
 
 gboolean ci_config_set(const gchar *key, gpointer value)
 {
-    if (g_strcmp0(key, "client:host") == 0) {
-        g_free(ci_config.host);
-        ci_config.host = g_strdup((gchar*)value);
+    if (g_str_has_prefix(key, "general:")) {
+        if (g_strcmp0(&key[8], "output") == 0) {
+            g_free(ci_config.general_output);
+            if (value != NULL)
+                ci_config.general_output = g_strdup((gchar*)value);
+            else
+                ci_config.general_output = g_strdup("default");
+        }
+        else
+            return FALSE;
     }
-    else if (g_strcmp0(key, "client:port") == 0) {
-        ci_config.port = GPOINTER_TO_UINT(value);
+    else if (g_str_has_prefix(key, "client:")) {
+        if (g_strcmp0(&key[7], "host") == 0) {
+            g_free(ci_config.client_host);
+            ci_config.client_host = g_strdup((gchar*)value);
+        }
+        else if (g_strcmp0(&key[7], "port") == 0) {
+            ci_config.client_port = GPOINTER_TO_UINT(value);
+        }
+        else if (g_strcmp0(&key[7], "retry-interval") == 0) {
+            ci_config.client_retry_interval = GPOINTER_TO_INT(value);
+        }
+        else if (g_strcmp0(&key[7], "user") == 0) {
+            ci_config.client_user = GPOINTER_TO_INT(value);
+        }
+        else
+            return FALSE;
     }
-    else if (g_strcmp0(key, "window:background") == 0) {
-        memcpy(&ci_config.background, value, sizeof(GdkRGBA));
-    }
-    else if (g_strcmp0(key, "window:x") == 0) {
-        ci_config.win_x = GPOINTER_TO_INT(value);
-    }
-    else if (g_strcmp0(key, "window:y") == 0) {
-        ci_config.win_y = GPOINTER_TO_INT(value);
-    }
-    else if (g_strcmp0(key, "window:width") == 0) {
-        ci_config.win_w = GPOINTER_TO_INT(value);
-    }
-    else if (g_strcmp0(key, "window:height") == 0) {
-        ci_config.win_h = GPOINTER_TO_INT(value);
-    }
-    else if (g_strcmp0(key, "client:retry-interval") == 0) {
-        ci_config.retry_interval = GPOINTER_TO_INT(value);
-    }
-    else if (g_strcmp0(key, "client:user") == 0) {
-        ci_config.user = GPOINTER_TO_INT(value);
+    else if (g_str_has_prefix(key, "window:")) {
+        if (g_strcmp0(&key[7], "background") == 0) {
+            memcpy(&ci_config.window_background, value, sizeof(GdkRGBA));
+        }
+        else if (g_strcmp0(&key[7], "x") == 0) {
+            ci_config.window_x = GPOINTER_TO_INT(value);
+        }
+        else if (g_strcmp0(&key[7], "y") == 0) {
+            ci_config.window_y = GPOINTER_TO_INT(value);
+        }
+        else if (g_strcmp0(&key[7], "width") == 0) {
+            ci_config.window_width = GPOINTER_TO_INT(value);
+        }
+        else if (g_strcmp0(&key[7], "height") == 0) {
+            ci_config.window_height = GPOINTER_TO_INT(value);
+        }
+        else
+            return FALSE;
     }
     else {
         return FALSE;
     }
+
     return TRUE;
 }
 
@@ -211,6 +259,9 @@ gboolean ci_config_load_root(JsonNode *node)
     if (!JSON_NODE_HOLDS_OBJECT(node))
         return FALSE;
     JsonObject *obj = json_node_get_object(node);
+    if (json_object_has_member(obj, "general") &&
+            !ci_config_load_general(json_object_get_member(obj, "general")))
+        return FALSE;
     if (json_object_has_member(obj, "client") &&
             !ci_config_load_client(json_object_get_member(obj, "client")))
         return FALSE;
@@ -223,6 +274,18 @@ gboolean ci_config_load_root(JsonNode *node)
     if (json_object_has_member(obj, "list") &&
             !ci_config_load_list(json_object_get_member(obj, "list")))
         return FALSE;
+    return TRUE;
+}
+
+gboolean ci_config_load_general(JsonNode *node)
+{
+    if (!JSON_NODE_HOLDS_OBJECT(node))
+        return FALSE;
+    JsonObject *obj = json_node_get_object(node);
+
+    if (json_object_has_member(obj, "output"))
+        ci_config_set("general:output", (gpointer)json_object_get_string_member(obj, "output"));
+
     return TRUE;
 }
 
@@ -389,6 +452,9 @@ JsonNode *ci_config_save_root(void)
     JsonNode *root = NULL;
 
     json_builder_begin_object(builder);
+    
+    json_builder_set_member_name(builder, "general");
+    ci_config_save_general(builder);
 
     json_builder_set_member_name(builder, "client");
     ci_config_save_client(builder);
@@ -411,21 +477,34 @@ JsonNode *ci_config_save_root(void)
     return root;
 }
 
+void ci_config_save_general(JsonBuilder *builder)
+{
+    json_builder_begin_object(builder);
+
+    json_builder_set_member_name(builder, "output");
+    if (ci_config.general_output != NULL)
+        json_builder_add_string_value(builder, ci_config.general_output);
+    else
+        json_builder_add_string_value(builder, "default");
+
+    json_builder_end_object(builder);
+}
+
 void ci_config_save_client(JsonBuilder *builder)
 {
     json_builder_begin_object(builder);
 
     json_builder_set_member_name(builder, "host");
-    json_builder_add_string_value(builder, ci_config.host);
+    json_builder_add_string_value(builder, ci_config.client_host);
 
     json_builder_set_member_name(builder, "port");
-    json_builder_add_int_value(builder, ci_config.port);
+    json_builder_add_int_value(builder, ci_config.client_port);
 
     json_builder_set_member_name(builder, "retry-interval");
-    json_builder_add_int_value(builder, ci_config.retry_interval);
+    json_builder_add_int_value(builder, ci_config.client_retry_interval);
 
     json_builder_set_member_name(builder, "user");
-    json_builder_add_int_value(builder, ci_config.user);
+    json_builder_add_int_value(builder, ci_config.client_user);
 
     json_builder_end_object(builder);
 }
@@ -435,18 +514,18 @@ void ci_config_save_window(JsonBuilder *builder)
     json_builder_begin_object(builder);
 
     json_builder_set_member_name(builder, "x");
-    json_builder_add_int_value(builder, ci_config.win_x);
+    json_builder_add_int_value(builder, ci_config.window_x);
 
     json_builder_set_member_name(builder, "y");
-    json_builder_add_int_value(builder, ci_config.win_y);
+    json_builder_add_int_value(builder, ci_config.window_y);
 
     json_builder_set_member_name(builder, "width");
-    json_builder_add_int_value(builder, ci_config.win_w);
+    json_builder_add_int_value(builder, ci_config.window_width);
 
     json_builder_set_member_name(builder, "height");
-    json_builder_add_int_value(builder, ci_config.win_h);
+    json_builder_add_int_value(builder, ci_config.window_height);
 
-    gchar *strcol = ci_color_to_string(&ci_config.background);
+    gchar *strcol = ci_color_to_string(&ci_config.window_background);
     json_builder_set_member_name(builder, "background");
     json_builder_add_string_value(builder, strcol);
     g_free(strcol);
